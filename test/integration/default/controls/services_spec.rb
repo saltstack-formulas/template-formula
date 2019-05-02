848c2ad2c76f5f107b7c12c0991145dbdfc1e26f
Imran Iqbal <iqbalmy@hotmail.com>
2019-05-02 08:41:46 +0100

    test(inspec): disable `service`-based tests for `opensuse-leap-15`

--- test/integration/default/controls/services_spec.rb
+++ test/integration/default/controls/services_spec.rb
@@ -1,3 +1,7 @@
+# Temporary `if` due to `opensuse-leap-15` bug re: `service`
+if os[:name] == 'suse'
+  puts "[Skip `service`-based tests due to `opensuse-leap-15` detection bug (see https://github.com/inspec/train/issues/377)]"
+else
 control 'Template service' do
   impact 0.5
   title 'should be running and enabled'
@@ -6,4 +10,5 @@ control 'Template service' do
     it { should be_enabled }
     it { should be_running }
   end
+end
 end
