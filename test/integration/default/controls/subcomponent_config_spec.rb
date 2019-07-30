fd55e03692f7ac9687d3d00da56854e7ed62ee7e
Daniel Dehennin <daniel.dehennin@ac-dijon.fr>
2019-07-30 16:06:53 +0200

    test(inspec): verify subcomponent configuration file
    * test/integration/default/controls/subcomponent_config_spec.rb: the
      subcomponent file must come from the “/subcomponent/files/” directory
    
    * test/integration/default/controls/config_spec.rb: update to match
      the new subcomponent override.

---
+++ test/integration/default/controls/subcomponent_config_spec.rb
@@ -0,0 +1,12 @@
+control 'template subcomponent configuration' do
+  title 'should match desired lines'
+
+  describe file('/etc/template-subcomponent-formula.conf') do
+    it { should be_file }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+    its('mode') { should cmp '0644' }
+    its('content') { should include '# File managed by Salt at <salt://template/subcomponent/files/default/subcomponent-example.tmpl.jinja>.' }
+    its('content') { should include 'This is another subcomponent example file from SaltStack template-formula.' }
+  end
+end
