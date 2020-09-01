97309c6f4d6b18723ec5492564b1344155960ae0
Imran Iqbal <iqbalmy@hotmail.com>
2020-09-01 18:24:12 +0100

    ci(kitchen+travis): fix `centos6` suite and rename to `upstart`
    * Semi-automated using https://github.com/myii/ssf-formula/pull/247
    * Problems identified while reviewing:
      - https://github.com/saltstack-formulas/template-formula/pull/203

--- test/integration/default/controls/services_spec.rb
+++ test/integration/default/controls/services_spec.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
-# Overide by OS
-service_name = 'systemd-journald'
-service_name = 'crond' if (os[:name] == 'centos') && os[:release].start_with?('6')
+# Prepare platform "finger"
+platform_finger = "#{platform[:name]}-#{platform[:release].split('.')[0]}"
 
 control 'TEMPLATE service' do
   impact 0.5
   title 'should be running and enabled'
 
+  # Overide by `platform_finger`
+  service_name =
+    case platform_finger
+    when 'centos-6', 'amazon-2018'
+      'crond'
+    else
+      'systemd-journald'
+    end
+
   describe service(service_name) do
+    it { should be_installed }
     it { should be_enabled }
     it { should be_running }
   end
